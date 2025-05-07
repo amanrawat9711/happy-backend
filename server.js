c3b2139748589b6a1dab5542cb1e1f7554a6960b
aman rawat <amanrawat9711@gmail.com>
2025-05-07 14:50:52 +0530

    ewrewr

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ connectCloudinary();
 
 app.use(express.json());
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://animation-front-eight.vercel.app/",
     credentials: true 
   }));
   
