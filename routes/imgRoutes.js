024c740ea19e5133e0bd8865181dd6511a66982f
aman rawat <amanrawat9711@gmail.com>
2025-05-07 15:13:19 +0530

    commited2

--- routes/imgRoutes.js
+++ routes/imgRoutes.js
@@ -5,7 +5,7 @@ import upload from '../middlewares/multer.js';
 const router = express.Router();
 
 // Endpoint to add multiple images (up to 8), requires imageType for each in body
-router.post("/images", upload.array("images", 8), addImages); // Changed endpoint to /images and used upload.array
+router.post("/images", upload.array("images", 15), addImages); // Changed endpoint to /images and used upload.array
 // Endpoint to get all images
 router.get("/get-image", getImage);
 
